074f43a4b4684df5846c5cfafb14bf2b2a25ecdd
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-07-10 16:00:35 +0200

    Update Hero.vue

--- golem-website/src/components/Hero.vue
+++ golem-website/src/components/Hero.vue
@@ -63,7 +63,7 @@
                 class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-golemblue text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                 @click="open = false"
               >
-                Read full documentation
+                Read the full documentation
               </button>
             </div>
           </div>
