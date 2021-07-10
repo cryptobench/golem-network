e46868384791c2ad0b25cd2c2ed585acac9da72d
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-07-10 15:38:02 +0200

    Add provider installation modal

--- golem-website/src/components/Hero.vue
+++ golem-website/src/components/Hero.vue
@@ -1,5 +1,77 @@
 <template>
-  <div class="relative bg-gray-50 overflow-hidden">
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" static class="fixed z-10 inset-0 overflow-y-auto" @close="open = false" :open="open">
+      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
+
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enter-to="opacity-100 translate-y-0 sm:scale-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100 translate-y-0 sm:scale-100"
+          leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div
+            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+          >
+            <div>
+              <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                <DownloadIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
+              </div>
+              <div class="mt-3 text-center sm:mt-5">
+                <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900">
+                  Let's onboard you in 1 minute!
+                </DialogTitle>
+                <div class="mt-2">
+                  <p class="text-sm text-gray-500">
+                    Installing Golem as a provider is very simple. On a Linux machine just copy and run the command below and voila! You're
+                    ready to provide.
+                  </p>
+
+                  <div class="mt-3 font-bold bg-gray-50 rounded">
+                    <p>curl -sSf https://join.golem.network/as-provider | bash -</p>
+                  </div>
+                </div>
+                <div class="mt-4">
+                  <p class="text-sm text-gray-500">
+                    Finally to start Golem just run the following
+                  </p>
+
+                  <div class="mt-3 font-bold bg-gray-50 rounded">
+                    <p>golemsp run</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="mt-5 sm:mt-6">
+              <button
+                type="button"
+                class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-golemblue text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                @click="open = false"
+              >
+                Read full documentation
+              </button>
+            </div>
+          </div>
+        </TransitionChild>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+  <div class="relative bg-white overflow-hidden">
     <div class="hidden lg:block lg:absolute lg:inset-0" aria-hidden="true">
       <svg
         class="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8"
@@ -18,7 +90,7 @@
       </svg>
     </div>
 
-    <div class="relative pb-16 sm:pb-24 lg:pb-32">
+    <div class="relative  pb-16 sm:pb-24 lg:pb-32">
       <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
         <div class="lg:grid lg:grid-cols-12 lg:gap-8">
           <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
@@ -35,8 +107,8 @@
             <div class="mt-5 max-w-md  sm:flex sm:justify-center md:mt-8">
               <div class="rounded-md shadow">
                 <a
-                  href="#"
-                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-golemblue hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                  @click="open = !open"
+                  class="w-full flex cursor-pointer items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-golemblue hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                 >
                   Start Providing
                 </a>
@@ -94,6 +166,9 @@
 </template>
 
 <script>
+import { ref } from "vue"
+import { Dialog, DialogOverlay, DialogTitle, TransitionChild, TransitionRoot } from "@headlessui/vue"
+import { DownloadIcon } from "@heroicons/vue/outline"
 const navigation = [
   { name: "Product", href: "#" },
   { name: "Features", href: "#" },
@@ -102,10 +177,19 @@ const navigation = [
 ]
 
 export default {
-  components: {},
+  components: {
+    Dialog,
+    DialogOverlay,
+    DialogTitle,
+    TransitionChild,
+    TransitionRoot,
+    DownloadIcon,
+  },
   setup() {
+    const open = ref(false)
     return {
       navigation,
+      open,
     }
   },
 }
