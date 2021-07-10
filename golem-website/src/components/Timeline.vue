c36f6685a36772308654b6890e2c80af74cf2fb4
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-07-10 15:38:28 +0200

    Add timeline

--- golem-website/src/components/Timeline.vue
+++ golem-website/src/components/Timeline.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+  <div class="relative h-full max-w-7xl mx-auto">
+    <svg
+      class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
+      width="404"
+      height="784"
+      fill="none"
+      viewBox="0 0 404 784"
+    >
+      <defs>
+        <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+          <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect width="404" height="784" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+    </svg>
+    <svg
+      class="absolute left-full transform -translate-x-1/4  lg:-translate-x-1/2"
+      width="404"
+      height="784"
+      fill="none"
+      viewBox="0 0 404 784"
+    >
+      <defs>
+        <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+          <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
+    </svg>
+  </div>
+  <div class="hidden lg:block lg:absolute lg:inset-0 z-0" aria-hidden="true">
+    <svg class="absolute top-0 left-1/2 transform  translate-y-32" width="640" height="784" fill="none" viewBox="0 0 640 784">
+      <defs>
+        <pattern id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+          <rect x="0" y="0" width="4" height="4" class="text-gray-200 z-0" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect y="72" width="640" height="640" class="text-gray-50 z-10" />
+      <rect x="118" width="404" height="784" fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)" />
+    </svg>
+  </div>
+  <div class="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-12 bg-gray-50 z-10 relative">
+    <h2 class="text-3xl font-bold tracking-tight sm:text-4xl text-golemblue mb-4">Our History</h2>
     <div class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
       <!-- left -->
       <div class="flex flex-row-reverse md:contents">
