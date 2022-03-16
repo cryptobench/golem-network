0854f1168eb0fe223e07431133caeee54fbb4631
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-03-16 15:24:47 +0100

    Hover animations on app

--- components/Apps.jsx
+++ components/Apps.jsx
@@ -32,7 +32,7 @@ export default function Apps() {
     <section className="bg-black  shadow-black relative apps ">
       <div className="absolute w-full h-full ">
         <div className="night ">
-          <div className="animate-me"></div>
+          <div className="animate-me "></div>
           <div className="animate-me"></div>
           <div className="animate-me"></div>
           <div className="animate-me"></div>
@@ -91,70 +91,70 @@ export default function Apps() {
             </div>
           </div>
           <div className="  col-span-8 bg-white px-4 py-2  grid grid-cols-12 gap-x-16 mt-4 rounded-r-lg rounded-bl-lg">
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/dapp/1924f1317b074d6e8a34ed8f21b4325b.blob"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">Blender</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/dapp/4244a929e00d467cbb7f3acb66f07ce5.blob"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">DeML</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/app/cdb2d057b5694a2a90f0be82e26a01ed.jpg"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">DeML</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/dapp/c74121a543544389b5dcb5bc59a39905.jpg"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">App 1</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/dapp/4121556bcff4421ea18a427895cee2ee.blob"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">App 1</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/dapp/23c1f984fbf94d3b99c6ae6905ecb05d.blob"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">App 1</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/app/9f65c47c-8c39-460b-9e4b-6fe1ce447fac.png"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">App 1</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/dapp/8dc747fa286642acb1b7ccbff7a33a9d.blob"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">App 1</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/dapp/46ade934a4624ae0ae6a89a8726cc3f7.blob"
               ></img>
               <p className="text-gray-900 text-md tracking-tight font-light">App 1</p>
             </div>
-            <div className="col-span-2 py-4 justify-items-center grid ">
+            <div className="col-span-2 py-4 justify-items-center grid app ">
               <img
                 className="h-16 w-16 rounded-lg shadow-lg"
                 src="https://dappimg.com/media/image/dapp/c915ac1844814c0f92f7b38ec4fd7d03.jpg"
