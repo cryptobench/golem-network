dc7eb0300986f8da6690958ec8d6b5b0502f6ece
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-05-11 16:53:56 +0200

    Globe changes

--- components/Globe.jsx
+++ components/Globe.jsx
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic"
 import Globe from "react-globe.gl"
 import { useEffect, useState, useRef } from "react"
 
-const MAP_CENTER = { lat: 37.6, lng: -16.6, altitude: 1.5 }
+const MAP_CENTER = { lat: 37.6, lng: -16.6, altitude: 1.6 }
 
 const Animate = () => {
   const globeEl = useRef()
@@ -13,7 +13,7 @@ const Animate = () => {
   useEffect(() => {
     // load data
     fetch(
-      "https://gist.githubusercontent.com/cryptobench/fc9f8791e8ec2197aaf14b1edf996abc/raw/5d5cbebc0efc3d7b84ca9f041dd0d107add1cf11/test.geojson"
+      "https://gist.githubusercontent.com/cryptobench/9bf07d45b5350b700fddcf2634b91e96/raw/d8bfb37ae4cdb3bd5b3bd4adbe0735267e9282cc/data.geojson"
     )
       .then((res) => res.json())
       .then(setCountries)
@@ -35,25 +35,33 @@ const Animate = () => {
       ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
     ],
   }))
-  const colors = ["#000000", "#ffffff"]
+  const colors = ["#0c14d4", "#ffffff"]
+
   return (
     <Globe
-      className="globelort"
       ref={globeEl}
       globeImageUrl="/earth.jpeg"
       atmosphereColor="#09238A"
+      polygonCapColor={() => "transparent"}
+      polygonSideColor={() => "transparent"}
+      polygonStrokeColor={true}
+      hexPolygonColor={() => "#0c14d4"}
+      polygonLabel={({ properties: d }) => `
+        <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+        Providers: <i>${Math.round(+d.POP_EST / 1e4) / 1e2}M</i>
+      `}
+      polygonsData={countries.features.filter((d) => d.properties.ISO_A2 !== "AQ")}
       atmosphereAltitude={0.13}
       backgroundColor="rgba(255, 0, 0, 0)"
       hexPolygonsData={countries.features}
       width={800}
       height={600}
       hexPolygonResolution={4}
-      pointOfView={MAP_CENTER}
-      hexPolygonMargin={0.1}
+      hexPolygonMargin={0.2}
       arcDashLength={() => Math.random()}
+      arcLabel="test"
       arcDashGap={() => Math.random()}
       arcDashAnimateTime={() => Math.random() * 4000 + 500}
-      hexPolygonColor={() => colors[Math.floor(Math.random() * colors.length)]}
       arcsData={arcsData}
       arcColor={"color"}
     />
