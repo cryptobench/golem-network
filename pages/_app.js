f6016aa38eb95db8c63db301db655c7d1c049823
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-06-26 21:42:01 +0200

    Framer motion page transition

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,21 @@ import "../styles/globals.css"
 import Head from "next/head"
 import Footer from "../components/Footer"
 import NextNProgress from "nextjs-progressbar"
+import { AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </Head>
       <NextNProgress color="#0c14d4" />
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
+        <motion.div key={router.pathname} exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
       <Footer></Footer>
     </>
   )
